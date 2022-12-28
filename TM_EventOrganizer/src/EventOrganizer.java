19340d522f9dc0d39c416a7791d6eb5d46e67754
Moch. Iqbal Firmansyah <65177974+iqbaalfrm@users.noreply.github.com>
2022-12-28 17:05:34 +0700

    Add files via upload

---
+++ TM_EventOrganizer/src/EventOrganizer.java
@@ -0,0 +1,77 @@
+import java.util.ArrayList;
+
+public class EventOrganizer {
+
+    private int CustId;
+    private String CustName;
+    private String CustAddr;
+    private String CustEmail;
+    private String CustPhone;
+
+
+    public int getCustId() {
+        return CustId;
+    }
+
+    public void setCustId(int custId) {
+        CustId = custId;
+    }
+
+    public String getCustName() {
+        return CustName;
+    }
+
+    public void setCustName(String custName) {
+        CustName = custName;
+    }
+
+    public String getCustAddr() {
+        return CustAddr;
+    }
+
+    public void setCustAddr(String custAddr) {
+        CustAddr = custAddr;
+    }
+
+    public String getCustEmail() {
+        return CustEmail;
+    }
+
+    public void setCustEmail(String custEmail) {
+        CustEmail = custEmail;
+    }
+
+    public String getCustPhone() {
+        return CustPhone;
+    }
+
+    public void setCustPhone(String custPhone) {
+        CustPhone = custPhone;
+    }
+
+    public EventOrganizer() {
+
+    }
+
+    public EventOrganizer(int custId, String custName, String custAddr, String custEmail, String custPhone) {
+    this.CustId = custId;
+    this.CustName = custName;
+    this.CustAddr = custAddr;
+    this.CustEmail = custEmail;
+    this.CustPhone = custPhone;
+    }
+
+    public void PrintGuests(ArrayList<EventOrganizer> guests){
+        for (EventOrganizer guest : guests){
+            System.out.println("======= Daftar Tamu =======");
+            System.out.println("Id Tamu : " + guest.getCustId());
+            System.out.println("Nama Tamu : " + guest.getCustName());
+            System.out.println("Alamat Tamu : " + guest.getCustAddr());
+            System.out.println("Email Tamu : " + guest.getCustEmail());
+            System.out.println("Nomer HP Tamu : " + guest.getCustPhone());
+            System.out.println("========================== ");
+            System.out.println("");
+
+        }
+    }
+}
---
+++ TM_EventOrganizer/src/GuestsRecord.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class GuestsRecord {
+    public static void main(String[] args) {
+        Scanner Input = new Scanner(System.in);
+        ArrayList<EventOrganizer> guests = new ArrayList<>();
+        System.out.println("Masukan Jumlah Tamu Undangan : ");
+        int Jumlah_Tamu = Input.nextInt();
+        for(int i = 0; i <Jumlah_Tamu; i++){
+            System.out.println("Tamu Ke - " + (i+1) + " : " );
+            System.out.println("Masukan ID Tamu : " );
+            int id = Input.nextInt();
+            Input.nextLine();
+            System.out.println("Masukan Nama : ");
+            String name = Input.nextLine();
+            System.out.println("Masukan Alamat : ");
+            String addr = Input.nextLine();
+            System.out.println("Masukan Email : ");
+            String email = Input.nextLine();
+            System.out.println("Masukan Nomor HP : ");
+            String phone = Input.nextLine();
+            System.out.println("");
+
+            EventOrganizer guest = new EventOrganizer(id, name,addr,email,phone);
+
+            guests.add(guest);
+
+            System.out.println("");
+            EventOrganizer eventOrganizer = new EventOrganizer(0 , "", "", "", "");
+            eventOrganizer.PrintGuests(guests);
+
+        }
+    }
+}
